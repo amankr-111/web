de91472a3d4cac70a474d2fe8f55e80aabb26519
Aman <amankr0741@gmail.com>
2023-08-27 23:42:06 +0530

    nav

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <li style={{width:'70px',height:'28px',fontSize:'18px',lineHeight:'21.11px',fontWeight:'400'}}><a href="#">ABOUT</a></li>
             <li class="dropdown" style={{width:'159px',height:'28px',fontWeight:'500',fontSize:'18px',lineHeight:'21.11px'}}>
                 <a href="#"><u>OUR PRODUCTS</u> </a>
-                <ul class="dropdown-content">
+                <ul class="dropdown-content" style={{border:"1px solid black"}}>
                     <li style={{width:'129px',height:'28px',fontWeight:'300',fontSize:'14px',lineHeight:'16.42px'}}><a href="#">PRODUCTS 1</a></li>
                     <li style={{width:'129px',height:'28px',fontWeight:'300',fontSize:'14px',lineHeight:'16.42px'}}><a href="#">PRODUCTS 2</a></li>
                     <li style={{width:'129px',height:'28px',fontWeight:'300',fontSize:'14px',lineHeight:'16.42px'}}><a href="#">PRODUCTS 3</a></li>
